094691495b7cb59b4686e255985237de5e54b9b9
ilyakostyrev <kostyrevila01@gmail.com>
2025-11-24 18:31:47 +0300

    bd_client crud api

--- tg_bot/main/bd_trainer/urls.py
+++ tg_bot/main/bd_trainer/urls.py
@@ -1,4 +1,4 @@
-from bd_trainer import views
+from . import views
 from django.urls import path
 # Trainer URLs
 urlpatterns = [
--- tg_bot/main/main/urls.py
+++ tg_bot/main/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('bd_trainer.urls')),
+    path('api/', include("bd_trainer.urls")),
+    path('api/', include("bd_client.urls")),
 ]
+# ImportError: cannot import name 'Сlient' from 'bd_client.models' (C:\Users\User\Desktop\tg_bot\tg_bot\main\bd_client\models.py) что за ошибка? 
